5dc16e5802ac73bcac22907ff2ff81325afe5b64
Stacy <stacykimilu@gmail.com>
2024-04-02 09:50:20 +0300

    dom modifying elements

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+  const mainElement = document.querySelector('main#main');
+     mainElement.remove();
+  const newHeader = document.createElement('h1');
+    newHeader.id = 'victory';
+    newHeader.textContent = 'stacy is the champion';
+  document.body.appendChild(newHeader);
